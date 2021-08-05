f65801608c550dabbf40c2da32fcd3cbdd1905e4
SBtree-bit <54784723+SBtree-bit@users.noreply.github.com>
2021-08-05 13:53:41 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (joining == true) {
         group.id = receivedNumber;
-radio.setGroup(group.id)
+        radio.setGroup(group.id)
         radio.sendNumber(Id)
         radio.onReceivedNumber(function (receivedNumber) {
             group.members[group.members.length] = receivedNumber;
@@ -13,7 +13,7 @@ radio.onReceivedString(function (receivedString) {
 })
 let joining = false
 let Id = 0
-Id = Math.random() * 254 + 1
+Id = Math.floor(Math.random() * 254 + 1);
 basic.showNumber(Id);
 let group = {
     id: 0,
@@ -23,7 +23,7 @@ radio.setGroup(1)
 while (true) {
     if (input.buttonIsPressed(Button.A) && group.id == 0) {
         group.id = Id;
-radio.setTransmitPower(3.5)
+        radio.setTransmitPower(2)
         radio.sendString("NewGroup")
         radio.sendNumber(group.id)
         radio.setGroup(group.id)
