70f62871358bce8ee2e5282a5e754b240017878f
SBtree-bit <54784723+SBtree-bit@users.noreply.github.com>
2021-08-15 15:22:40 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,36 +1,30 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    if (joining == true) {
-        group.id = receivedNumber;
-        radio.setGroup(group.id)
-        radio.sendNumber(Id)
-        radio.onReceivedNumber(function (receivedNumber) {
-            group.members[group.members.length] = receivedNumber;
-            basic.showNumber(receivedNumber);
-        })
-    }
-})
-radio.onReceivedString(function (receivedString) {
-    joining = true
-})
-let joining = false
-let Id = 0
-Id = Math.floor(Math.random() * 254 + 1);
-basic.showNumber(Id);
-let group = {
-    id: 0,
-    members: [Id]
-}
-radio.setGroup(1)
+let exit = false
+let buttonPressed = false
+let groupId = 0
+let myId = Math.floor(Math.random() * 100000)
+
+basic.showNumber(myId);
+
 while (true) {
-    if (input.buttonIsPressed(Button.A) && group.id == 0) {
-        group.id = Id;
-        radio.setTransmitPower(2)
-        radio.sendString("NewGroup")
-        radio.sendNumber(group.id)
-        radio.setGroup(group.id)
-        radio.onReceivedNumber(function (receivedNumber) {
-            group.members[group.members.length] = receivedNumber;
-            basic.showNumber(receivedNumber)
-        })
+    let joinGroup = 0
+    if (!(joinGroup)) {
+        if (input.buttonIsPressed(Button.A) && !(buttonPressed)) {
+            buttonPressed = true
+            for (let index = 0; index < 500; index++) {
+                for (let index = 0; index < 100000; index++) {
+                    if (!(input.buttonIsPressed(Button.A))) {
+                        exit = true
+                        break;
+                    }
+                }
+                if (exit) {
+                    break;
+                }
+            }
+            if (!(exit)) {
+                basic.showString("A")
+            }
+            buttonPressed = false
+        }
     }
 }
