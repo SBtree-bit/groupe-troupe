76bba57ce7a5b404841708ee28dab11624f9fbca
SBtree-bit <54784723+SBtree-bit@users.noreply.github.com>
2021-10-11 21:56:21 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,60 +1,13 @@
-let exit = false
-let buttonPressed = false
-let groupId = 0
-let myId = Math.floor(Math.random() * 100000)
-radio.setGroup(1);
-
-function join() {
-    basic.showString("Check for groups");
-    for (let index = 0; index < 10000; index++) {
-        for (let index = 0; index < 100000; index++) {
-            radio.onReceivedValue(function (name, value) {
-                if (name == "NewGroup") {
-                    groupId = value;
-                }
-            });
-            if (groupId != 0) {
-                exit = true
-                break;
-            }
-        }
-        if (exit) {
-            break;
-        }
+radio.onReceivedNumber(function (receivedNumber) {
+    if (radio.receivedPacket(RadioPacketProperty.SignalStrength) < (-128 + (distance * 11))) {
+        music.playTone(392, music.beat(BeatFraction.Whole))
+        basic.showIcon(IconNames.Skull)
     }
-    if (!exit)
-    {
-        basic.showString("New Group");
-        groupId = myId;
-        radio.sendNumber(0);
-        for (let index = 0; index < 10000; index++) {
-            for (let index = 0; index < 100000; index++) {
-                radio.sendValue("NewGroup", myId);
-            }
-        }
-    }
-}
-
-while (true) {
-    let joinGroup = 0
-    if (!(joinGroup)) {
-        if (input.buttonIsPressed(Button.A) && !(buttonPressed)) {
-            buttonPressed = true
-            for (let index = 0; index < 500; index++) {
-                for (let index = 0; index < 100000; index++) {
-                    if (!(input.buttonIsPressed(Button.A))) {
-                        exit = true
-                        break;
-                    }
-                }
-                if (exit) {
-                    break;
-                }
-            }
-            if (!(exit)) {
-                join();
-            }
-            buttonPressed = false
-        }
-    }
-}
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(0)
+})
+let distance = 0
+distance = 5
+radio.setGroup(0)
+basic.showIcon(IconNames.Heart)
