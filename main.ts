9ad40eb9e6625c549293c693f9d208613dc5033a
SBtree-bit <54784723+SBtree-bit@users.noreply.github.com>
2021-10-14 21:52:54 -0700

    Restore '10/11/2021, 9:56:21 PM Update main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,42 +1,13 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (radio.receivedPacket(RadioPacketProperty.SignalStrength) < -128 + distance * 11) {
-        music.playTone(392, music.beat(BeatFraction.Sixteenth))
+    if (radio.receivedPacket(RadioPacketProperty.SignalStrength) < (-128 + (distance * 11))) {
+        music.playTone(392, music.beat(BeatFraction.Whole))
         basic.showIcon(IconNames.Skull)
-    } else {
-        basic.showIcon(IconNames.Heart)
     }
 })
-
 input.onButtonPressed(Button.A, function () {
-    distance--
-    basic.showNumber(distance)
-    if (distance < 1) {
-        distance = 1
-    }
+    radio.sendNumber(0)
 })
-input.onButtonPressed(Button.B, function() {
-    distance++
-    basic.showNumber(10 - distance);
-    if (distance > 10) {
-        distance = 10
-    }
-})
-
 let distance = 0
-distance = 3
+distance = 5
 radio.setGroup(0)
 basic.showIcon(IconNames.Heart)
-let hot_cold = false
-
-basic.forever(function () {
-    radio.sendNumber(0)
-    if (hot_cold) {
-        if (radio.receivedPacket(RadioPacketProperty.SignalStrength) <= -95) {
-            basic.showIcon(IconNames.SmallDiamond)
-        } else if (radio.receivedPacket(RadioPacketProperty.SignalStrength) <= -80) {
-            basic.showIcon(IconNames.Diamond)
-        } else {
-            basic.showIcon(IconNames.Square)
-        }
-    }
-})
