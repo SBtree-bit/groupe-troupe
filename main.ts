3cc8af03fc254136c87d49c0dcd5c3f26d97a244
SBtree-bit <54784723+SBtree-bit@users.noreply.github.com>
2021-10-16 19:56:27 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,8 @@
-radio.onReceivedNumber(function () {
-    if ((radio.receivedPacket(RadioPacketProperty.SignalStrength) < (-128 + (distance * 11))) && !hot_cold) {
+radio.onReceivedNumber(function (receivedNumber) {
+    if ((radio.receivedPacket(RadioPacketProperty.SignalStrength) < -128 + distance * 11) && !hot_cold) {
         music.playTone(392, music.beat(BeatFraction.Whole))
         basic.showIcon(IconNames.Skull)
-    }
-})
-
-radio.onReceivedNumber(function () {
-    if (hot_cold) {
+    } else if (hot_cold) {
         if (radio.receivedPacket(RadioPacketProperty.SignalStrength) < (-128 + (1 * 11))) {
             basic.showIcon(IconNames.SmallDiamond)
         } else if (radio.receivedPacket(RadioPacketProperty.SignalStrength) < (-128 + (5 * 11))) {
@@ -17,28 +13,29 @@ radio.onReceivedNumber(function () {
     }
 })
 
-loops.everyInterval(1, function () {
-    radio.sendNumber(0);
+input.onButtonPressed(Button.AB, function() {
+    hot_cold = !hot_cold
 })
 
-input.onButtonPressed(Button.A, function() {
-    distance--
+input.onButtonPressed(Button.A, function () {
+    distance += -1
     if (distance < 1) {
         distance = 1
     }
     basic.showNumber(distance)
 })
-
-input.onButtonPressed(Button.B, function() {
-    distance++
+input.onButtonPressed(Button.B, function () {
+    distance += 1
     if (distance > 10) {
         distance = 10
     }
     basic.showNumber(distance)
 })
-
 let distance = 0
 let hot_cold = false
 distance = 5
 radio.setGroup(0)
 basic.showIcon(IconNames.Heart)
+loops.everyInterval(1, function () {
+    radio.sendNumber(0)
+})
