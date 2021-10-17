0a2ae51cd0dfe0717150ba23479c33455a04fa8f
SBtree-bit <54784723+SBtree-bit@users.noreply.github.com>
2021-10-16 20:12:47 -0700

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (radio.receivedPacket(RadioPacketProperty.SignalStrength) < -128 + distance * 11 && !(hot_cold)) {
-        music.playTone(392, music.beat(BeatFraction.Whole))
-        basic.showIcon(IconNames.Skull)
+    if (!hot_cold) {
+        if ((radio.receivedPacket(RadioPacketProperty.SignalStrength) < -128 + distance * 11)) {
+            music.playTone(392, music.beat(BeatFraction.Whole))
+            basic.showIcon(IconNames.Skull)
+        }
     } else {
         if (radio.receivedPacket(RadioPacketProperty.SignalStrength) < (-128 + (2 * 11))) {
             basic.showIcon(IconNames.SmallDiamond)
@@ -12,6 +14,7 @@ radio.onReceivedNumber(function (receivedNumber) {
         }
     }
 })
+
 input.onButtonPressed(Button.A, function () {
     distance += -1
     if (distance < 1) {
@@ -19,9 +22,11 @@ input.onButtonPressed(Button.A, function () {
     }
     basic.showNumber(distance)
 })
+
 input.onButtonPressed(Button.AB, function () {
     hot_cold = !(hot_cold)
 })
+
 input.onButtonPressed(Button.B, function () {
     distance += 1
     if (distance > 10) {
@@ -29,9 +34,10 @@ input.onButtonPressed(Button.B, function () {
     }
     basic.showNumber(distance)
 })
+
 let hot_cold = false
 let distance = 0
-distance = 5
+distance = 1
 radio.setGroup(0)
 basic.showIcon(IconNames.Heart)
 loops.everyInterval(1, function () {
